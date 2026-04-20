5cee10494875ff0fee458dd68a6d27f0cf592b62
UrvashiPandey <urvashipandey20001905@gmail.com>
2026-04-20 09:29:58 +0530

    Added Tic-Tac-Toe UC1 Java implementation

--- TicTacToe-App/src/Main.java
+++ TicTacToe-App/src/Main.java
@@ -1,13 +1,30 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+#include <iostream>
+using namespace std;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+void initializeBoard(char board[3][3]) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            board[i][j] = '-';
+        }
     }
+}
+
+void displayBoard(char board[3][3]) {
+    cout << "\nTic-Tac-Toe Board:\n";
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << board[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    char board[3][3];
+
+    initializeBoard(board);
+
+    displayBoard(board);
+
+    return 0;
 }
---
+++ TicTacToe-App/src/TicTacToe.java
@@ -0,0 +1,28 @@
+public class TicTacToe {
+
+    public static void initializeBoard(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    public static void displayBoard(char[][] board) {
+        System.out.println("\nTic-Tac-Toe Board:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+
+        initializeBoard(board);
+
+        displayBoard(board);
+    }
+}
